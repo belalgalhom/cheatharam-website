c75d72008ea662e720263b32e3fa5e298dfb6e51
belalgalhom <113742499+belalgalhom@users.noreply.github.com>
2026-04-29 03:15:16 +0300

    yes

--- src/views/ScreenshotsView.vue
+++ src/views/ScreenshotsView.vue
@@ -256,13 +256,7 @@ const toggleZoom = (e: Event) => {
       >
         <div class="fixed top-8 right-8 flex items-center gap-4 z-[110]">
           <button
-            @click.stop="
-              downloadImage(
-                screenshots[selectedIndex!]?.url,
-                screenshots[selectedIndex!]?.player,
-                screenshots[selectedIndex!]?.date,
-              )
-            "
+            @click.stop="downloadImage(ss.url)"
             class="p-3 bg-white/10 text-white rounded-xl hover:bg-emerald-500 hover:text-black transition-colors backdrop-blur-md shadow-2xl"
           >
             <Download class="w-6 h-6" />
